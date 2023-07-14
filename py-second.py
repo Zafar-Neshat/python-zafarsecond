633035d9ff76b4268012f8e0afce42a9eaef1fdf
zafar neshat <zafarnishat1381@gmail.com>
2023-07-14 19:34:55 +0430

    tthis is training for making git hub

---
+++ py-second.py
@@ -0,0 +1,17 @@
+if 30 < 20:
+    print("true")
+
+if 965 < 562:
+    print("nine hundred sixty five is smaller than five hundred sixty two")
+
+if 150 > 100:
+    print("True")       
+
+if 200 > 66:
+    print("Two hundred is bigger than sixty six")
+
+if 20 == 20:
+    print("yes")
+
+if 7 == 7:
+    print("equal")
